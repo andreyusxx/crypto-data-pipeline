fd3c44dbef9758a6ddf8297630d527f8a8651312
Шарагін Андрій <andrijsaragin@gmail.com>
2026-02-16 13:06:01 +0200

    database: move coin-specific table definitions to init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -1,6 +1,59 @@
-CREATE TABLE IF NOT EXISTS bitcoin_prices (
-    id SERIAL PRIMARY KEY,            
-    symbol VARCHAR(10),               
-    price DECIMAL(18, 8),             
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
+CREATE TABLE IF NOT EXISTS prices_btc (
+    id SERIAL PRIMARY KEY,
+    price DECIMAL(18, 8),
+    volume DECIMAL(18, 8),
+    event_time BIGINT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS prices_eth (
+    id SERIAL PRIMARY KEY,
+    price DECIMAL(18, 8),
+    volume DECIMAL(18, 8),
+    event_time BIGINT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS prices_sol (
+    id SERIAL PRIMARY KEY,
+    price DECIMAL(18, 8),
+    volume DECIMAL(18, 8),
+    event_time BIGINT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS prices_bnb (
+    id SERIAL PRIMARY KEY,
+    price DECIMAL(18, 8),
+    volume DECIMAL(18, 8),
+    event_time BIGINT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS prices_ada (
+    id SERIAL PRIMARY KEY,
+    price DECIMAL(18, 8),
+    volume DECIMAL(18, 8),
+    event_time BIGINT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS prices_doge (
+    id SERIAL PRIMARY KEY,
+    price DECIMAL(18, 8),
+    volume DECIMAL(18, 8),
+    event_time BIGINT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS prices_xrp (
+    id SERIAL PRIMARY KEY,
+    price DECIMAL(18, 8),
+    volume DECIMAL(18, 8),
+    event_time BIGINT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE TABLE IF NOT EXISTS prices_dot (
+    id SERIAL PRIMARY KEY,
+    price DECIMAL(18, 8),
+    volume DECIMAL(18, 8),
+    event_time BIGINT,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
